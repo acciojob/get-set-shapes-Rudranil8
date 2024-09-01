9b8dd15fb97bad9bb96cd726b6196b589988e948
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-09-01 22:22:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-//complete this code
-class Rectangle {}
+// class Rectangle {}
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    get width() {
+        return this.width;
+    }
+
+    get height() {
+        return this.height;
+    }
+}
 
 class Square extends Animal {}
+
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
